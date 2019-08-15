677fce26a99b2e726c652cd968ceaafb1389a7d0
NAVEED MASTAN <m.a.naveedmastan@gmail.com>
2019-08-15 21:59:06 +0530

    changed text

--- src/helpers/Display.js
+++ src/helpers/Display.js
@@ -11,7 +11,7 @@ export const Display = props => {
           }
           onClick={() => changeType("beautify")}
         >
-          View Beautify
+          Beautify
         </button>
         <button
           className={
